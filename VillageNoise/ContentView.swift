0d77cd424064264742856dea3dcd7aa866122cf8
PieterNatanael <pieternatanael@icloud.com>
2023-12-01 13:33:57 +0800

    Initial Commit

---
+++ VillageNoise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VillageNoise
+//
+//  Created by Pieter Yoshua Natanael on 01/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VillageNoise/VillageNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VillageNoiseApp.swift
+//  VillageNoise
+//
+//  Created by Pieter Yoshua Natanael on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct VillageNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
